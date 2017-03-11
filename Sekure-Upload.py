02e3b1fbff018d08e1829ef1c511486a404e043a
brown4224 <swmcglincy@hotmail.com>
2017-03-10 23:46:34 -0500

    Uploader working

--- Sekure-Upload.py
+++ Sekure-Upload.py
@@ -1,39 +1,43 @@
 import os
 import datetime
 import boto3
+import magic
 import config
 import re
 
+'''
+Step 1:  Run 'yum update -y'
 
-# Run 'yum update -y'
-# System Requirments: Install PIP for Python using their repo
-# Then install boto3: AWS plugin
-# sudo pip install boto3
-#  https://github.com/boto/boto3
+Step 2: System Requirements: Install PIP for Python using their repo
 
 
-#  Requires:  Make a 'config.py' file with aws creds
-# aws_id=
-# aws_secret_access_key=
-# aws_s3=
+Step 3: sudo pip install boto3
+Then install boto3: AWS plugin
+https://github.com/boto/boto3
 
+
+Step 4: sudo pip install python-magic
+Mime is controled by the 'magic' plugin
+https://github.com/ahupp/python-magic
+
+
+Step 5:  Make a 'config.py' file with aws creds
+config.py
+    aws_id=''
+    aws_secret=''
+    aws_s3=''
+'''
+
+#  AWS Creds
 id = config.aws_id
-secret = config.aws_secret_access_key
+secret = config.aws_secret
 bucketName = config.aws_s3
+max_file_size = 100 * 1024   # Kilo Bytes
 
 
-#  Variables
-real_path = os.path.realpath(__file__)  # Gets Current File
-current_dir = os.path.dirname(real_path)  # Gets Current Directory
-upload_dir = current_dir + "/upload/"
-time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-archived = current_dir + "/archived" + "-" + time_stamp + "/"
-
-
-
-def check_folder(folder_name):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
+def check_folder(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 def move_files(file_path,  new_name):
     new_path = re.sub(file_path, new_name, file_path)
@@ -43,31 +47,57 @@ def move_files(file_path,  new_name):
 def recurse(base_folder):
     for root, directories, filenames in os.walk(base_folder):
         for directory in directories:
-            dir = os.path.join(root, directory)
-            # print dir
+            os.path.join(root, directory)
 
         for filename in filenames:
             current_file = os.path.join(root, filename)
-            aws_path = current_file + "-" +time_stamp
-            aws_path = re.sub(upload_dir, "", aws_path)
-            print current_file
-            print aws_path
+            if os.path.getsize(current_file) > max_file_size:
+                print "File too large to upload"
+                print "Check Max upload size"
+                break
+
+            # No folder structure
+            time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            aws_path = filename + "-" + time_stamp
+
+            # Keeps folder structure
+            # aws_path = current_file + "-" + time_stamp
+            # aws_path = re.sub(upload_dir, "", aws_path)
             upload(current_file, aws_path)
+
+
 
 # This Function uploads
 def upload(file_path, aws_path):
+    mime = magic.from_file(file_path, mime=True)
+
     s3 = boto3.resource('s3',
                         aws_access_key_id=id,
                         aws_secret_access_key=secret)
+
     data = open(file_path, 'rb')
-    s3.Bucket(bucketName).put_object(Key=aws_path, Body=data, ContentType='text/plain',
+    s3.Bucket(bucketName).put_object(Key=aws_path, Body=data, ContentType=mime,
                                      ServerSideEncryption='AES256')
+    print "Upload complete"
+    print "File path: " + file_path
+    print "AWS path: " + aws_path
+    print "MIME: " + mime
+    print ""
 
 
-check_folder(upload_dir)
-recurse(upload_dir)
-move_files(upload_dir, archived)
-check_folder(upload_dir)
+def main():
+#  Variables
+    real_path = os.path.realpath(__file__)  # Gets Current File
+    current_dir = os.path.dirname(real_path)  # Gets Current Directory
+    upload_dir = current_dir + "/upload/"
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    archived = current_dir + "/archived" + "-" + time_stamp + "/"
 
 
+    check_folder(upload_dir)
+    recurse(upload_dir)
+    move_files(upload_dir, archived)
+    check_folder(upload_dir)
+
+main()
 
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-aws_id="AKIAJDVKBBFUICBZFPJA",
-aws_secret_access_key="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"
-aws_s3="sekure-archive"
+aws_id=""
+aws_secret=""
+aws_s3=""
